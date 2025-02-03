0e9acfd26805ae4eafd6395388fed65485ebb015
jinvk <jinvk.to@gmail.com>
2025-02-03 22:51:20 +0900

    ehotsk?

---
+++ dnjf/eos/beat.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import pickle
+from plot import scatter_b0, scatter_del_E
+
+import sys
+
+from util import *
+# from log import *
+
+def open_jar(df):
+    with open(df, 'rb') as f:
+        pkl = pickle.load(f)
+    return pkl
+
+def clean_df(df, b0 = False):
+    rmv = []
+    for col in df.columns:
+        if 'fit' in col or 'rel' in col or 'vol' in col or 'pe' in col:
+            rmv.append(col)
+    rmv.append('mp_id')
+    if b0:
+        rmv.append('del_E')
+    else:
+        rmv.append('b0')
+    df2=df.drop(columns=rmv,inplace=False)
+    df2.reset_index(drop=True,inplace=True)
+    return df2
+
+def get_pickles(system):
+    df1=os.path.join(os.environ['JAR'], f'{system}_fccbcc_fit.pkl')
+    df2=os.path.join(os.environ['JAR'], f'{system}_fcchcp_fit.pkl')
+    df3=os.path.join(os.environ['JAR'], f'{system}_hcpbcc_fit.pkl')
+    pkl1 = open_jar(df1)
+    pkl2 = open_jar(df2)
+    pkl3 = open_jar(df3)
+    return pkl1, pkl2, pkl3
+
+def get_b0_df(system):
+    fb, fh, hb = get_pickles(system)
+    df = pd.concat([fb, fh, hb])
+    df = clean_df(df, b0=True)
+    df = df.drop_duplicates('bravais', keep='first')
+    return df
+
+def get_b0_tot(systems):
+    dfs = []
+    for system in systems:
+        df = get_b0_df(system)
+        dfs.append(df)
+    tot_jar = pd.concat(dfs)
+    tot_jar.rename(columns={'del_b0-dft':'b0-dft'},inplace=True)
+    return tot_jar
+
+def get_del_df(system):
+    fb, fh, hb = get_pickles(system)
+    fb['bravais'] = 'FCC-BCC'
+    fh['bravais'] = 'FCC-HCP'
+    hb['bravais'] = 'HCP-BCC'
+    fb.drop(1, inplace=True)
+    fh.drop(1, inplace=True)
+    hb.drop(1, inplace=True)
+    df = pd.concat([fb, fh, hb])
+    df = clean_df(df)
+    return df
+
+def convert_eV(df):
+    eVs = []
+    for col in df.columns:
+        if 'del_E' in col:
+            eVs.append(col)
+    for eV in eVs:
+        df[eV] = df[eV].apply(lambda x: x*1000)
+    return df
+
+def get_del_tot(systems):
+    dfs = []
+    for system in systems:
+        df = get_del_df(system)
+        dfs.append(df)
+    tot_jar = pd.concat(dfs)
+    return tot_jar
+
+def main(pbe):
+    set_env(task='eos',pbe=pbe) 
+    
+    systems = ['Au','Ag','Al','Ca','Co','Cu','Fe','Hf','Ir','K','Li','Mg','Mo','Na','Nb','Ni','Os','Pd','Pt','Re','Rb','Rh','Sr','Ti','Ta','V','W','Zn','Zr','Cs'] #Y, Cd
+    mlps = ['chgTot','chgTot_l3i3','chgTot_l3i5','chgTot_l4i3','m3g_n','m3g_r6','m3g_r55','omat_epoch1','omat_epoch2','omat_epoch3','omat_epoch4','omat_ft_r5','r5pp','omat_i5pp_epoch1','omat_i5pp_epoch2','omat_i5pp_epoch3','omat_i5pp_epoch4','omat_i5_epoch1','omat_i5_epoch2','omat_i5_epoch3','omat_i5_epoch4','omat_i3pp','mace','matsim']
+    tot_b0 = get_b0_tot(systems)
+    tot_del = get_del_tot(systems)
+    tot_del = convert_eV(tot_del)
+    
+    save_dict(tot_b0, os.path.join(os.environ['JAR'], 'tot_b0.pkl'))
+    save_dict(tot_del, os.path.join(os.environ['JAR'], 'tot_del.pkl'))
+    
+    for mlp in mlps:
+        scatter_b0(tot_b0, mlp)
+        scatter_del_E(tot_del, mlp) 
+        
+if __name__ == '__main__':
+    main(pbe=sys.argv[1])
--- dnjf/eos/plot.py
+++ dnjf/eos/plot.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-import pickle
 import numpy as np
-import pandas as pd
 import os
-
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 from util import make_dir 
 
 def eos_plot(system, df, mlp):
@@ -54,3 +52,135 @@ def eos_plot(system, df, mlp):
     path=make_dir(os.path.join(os.environ['PLOT'],'eos',system.lower()), return_path=True)
     plt.savefig(os.path.join(path,f'{system}_{tag}-{mlp}.png'))
     plt.close(fig)
+    
+
+def scatter_b0(df, mlp, return_scores=False): #TODO: group with elements
+    colors = ['#4265ff','#202c7c','#9db0c2','#0032fa','#fa325a','#135718','#78a5bf','#424f8a','#d1200d','#6fa8e8','#638545',
+              '#d4686b','#006394','#01248c','#801f18','#417027','#1dccb8','#2f0d94','#4265ff']
+    
+    system = df['system'].to_list()[0] 
+    fcc_mask = df['bravais'] == 'fcc'
+    hcp_mask = df['bravais'] == 'hcp'
+    bcc_mask = df['bravais'] == 'bcc'
+    fcc = df[fcc_mask]
+    hcp = df[hcp_mask]
+    bcc = df[bcc_mask]
+     
+    fcc_mae = mean_absolute_error(fcc['b0-dft'],fcc[f'b0-{mlp}'])
+    fcc_r2 = r2_score(fcc['b0-dft'],fcc[f'b0-{mlp}'])
+    hcp_mae = mean_absolute_error(hcp['b0-dft'],hcp[f'b0-{mlp}'])
+    hcp_r2 = r2_score(hcp['b0-dft'],hcp[f'b0-{mlp}'])
+    bcc_mae = mean_absolute_error(bcc['b0-dft'],bcc[f'b0-{mlp}'])
+    bcc_r2 = r2_score(bcc['b0-dft'],bcc[f'b0-{mlp}'])
+    mae = mean_absolute_error(df['b0-dft'],df[f'b0-{mlp}'])
+    r2 = r2_score(df['b0-dft'],df[f'b0-{mlp}'])
+     
+
+    fig, axs = plt.subplots(figsize=(6.5,6.5))
+    axs.set_title(mlp, loc='right',fontsize=17, fontweight='bold', pad=10)
+    x=np.arange(-100,500,0.1)
+    y=np.arange(-100,500,0.1)
+    
+    axs.plot(x,y,color = colors[2], linestyle='--', linewidth=2, zorder=1)
+    
+    axs.scatter(fcc['b0-dft'],fcc[f'b0-{mlp}'],label='FCC', color='#4a6cf2', marker='X', edgecolors='k', s=240, alpha=0.8, zorder=3)
+    axs.scatter(hcp['b0-dft'],hcp[f'b0-{mlp}'],label=f'HCP', color='#2dc9b7', marker='h', edgecolors='k', s=360, alpha=1, zorder=2)
+    axs.scatter(bcc['b0-dft'],bcc[f'b0-{mlp}'],label=f'BCC', color='#319bd8', marker='D', edgecolors='k', s=200, alpha=0.8, zorder=1)
+    
+    axs.set_ylabel("MLP bulk modulus (GPa)", fontsize=19, labelpad=0, fontweight='bold')
+    axs.set_xlabel('DFT bulk modulus (GPa)', fontsize=17, labelpad=-1, fontweight='bold') 
+    axs.tick_params( axis="x", direction="in", width=2.5, length=9, labelsize=17, pad=4) 
+    axs.tick_params( axis="y", direction="in", width=2.5, length=9, labelsize=19, pad=4)  
+
+    axs.legend(fontsize=12, ncol=3, frameon=True, loc='upper left', bbox_to_anchor=(0.0, 1.0))
+    axs.spines['top'].set_linewidth(4)
+    axs.spines['bottom'].set_linewidth(4) 
+    axs.spines['left'].set_linewidth(4)
+    axs.spines['right'].set_linewidth(4)    
+    axs.set_box_aspect(1)
+
+    # axs.text(-45,41,f'MAE: {l2i5_E_mae:.2f}',verticalalignment='top', horizontalalignment='left', fontsize=10, color='black')
+    # axs.text(-45, 48, fr'$R^2$: {l2i5_E_r2:.2f}', verticalalignment='top', horizontalalignment='left', fontsize=10, color='black')
+    axs.annotate(fr'FCC  MAE: {fcc_mae:.2f},   $R^2$: {fcc_r2:.2f}',(0.97,0.22),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')
+    axs.annotate(fr'HCP  MAE: {hcp_mae:.2f},   $R^2$: {hcp_r2:.2f}',(0.97,0.17),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')
+    axs.annotate(fr'BCC  MAE: {bcc_mae:.2f},   $R^2$: {bcc_r2:.2f}',(0.97, 0.12),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')    
+    axs.annotate(fr'TOT  MAE: {mae:.2f},   $R^2$: {r2:.2f}',(0.97,0.07),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')
+
+
+    tag=f"{df['bravais'].to_list()[0]}{df['bravais'].to_list()[1]}"
+    
+    axs.set_xlim(-50,500)
+    axs.set_ylim(-50,500)
+    fig.set_layout_engine('tight')
+    
+    path=make_dir(os.path.join(os.environ['PLOT'],'b0',system.lower()), return_path=True)
+    fig.savefig(os.path.join(path,f'{system.lower()}+{tag}_{mlp}_b0.png'))
+    if return_scores:
+        return mae, r2
+    return
+
+
+def scatter_del_E(df, mlp, return_scores=False): #TODO: group with elements
+    colors = ['#4265ff','#202c7c','#9ddel_Ec2','#0032fa','#fa325a','#135718','#78a5bf','#424f8a','#d1200d','#859cb7','#638545',
+              '#d4686b','#006394','#01248c','#801f18','#417027','#1dccb8','#2f0d94','#4265ff']
+    
+    system = df['system'].to_list()[0] 
+    fb_mask = df['bravais'] == 'FCC-BCC'
+    fh_mask = df['bravais'] == 'FCC-HCP'
+    hb_mask = df['bravais'] == 'HCP-BCC'
+    fb = df[fb_mask]
+    fh = df[fh_mask]
+    hb = df[hb_mask]
+     
+    fb_mae = mean_absolute_error(fb['del_E-dft'],fb[f'del_E-{mlp}'])
+    fb_r2 = r2_score(fb['del_E-dft'],fb[f'del_E-{mlp}'])
+    fh_mae = mean_absolute_error(fh['del_E-dft'],fh[f'del_E-{mlp}'])
+    fh_r2 = r2_score(fh['del_E-dft'],fh[f'del_E-{mlp}'])
+    hb_mae = mean_absolute_error(hb['del_E-dft'],hb[f'del_E-{mlp}'])
+    hb_r2 = r2_score(hb['del_E-dft'],hb[f'del_E-{mlp}'])
+    mae = mean_absolute_error(df['del_E-dft'],df[f'del_E-{mlp}'])
+    r2 = r2_score(df['del_E-dft'],df[f'del_E-{mlp}'])
+     
+
+    fig, axs = plt.subplots(figsize=(6.5,6.5))
+    axs.set_title(mlp, loc='right',fontsize=17, fontweight='bold', pad=10)
+    x=np.arange(-3000,3000,0.1)
+    y=np.arange(-3000,3000,0.1)
+    
+    axs.plot(x,y,color = '#859cb7', linestyle='--', linewidth=2, zorder=1)
+    
+    axs.scatter(fb['del_E-dft'],fb[f'del_E-{mlp}'],label='FCC-BCC', color='#526fff', marker='^', edgecolors='k', s=240, alpha=1, zorder=1)
+    axs.scatter(fh['del_E-dft'],fh[f'del_E-{mlp}'],label=f'FCC-HCP', color='#ff5e00', marker='^', edgecolors='k', s=240, alpha=.8, zorder=3)
+    axs.scatter(hb['del_E-dft'],hb[f'del_E-{mlp}'],label=f'HCP-BCC', color='#00aeff', marker='^', edgecolors='k', s=240, alpha=0.8, zorder=2)
+    axs.set_ylabel(fr'MLP $\Delta$E (meV)', fontsize=19, labelpad=0, fontweight='bold')
+    axs.set_xlabel(fr'DFT $\Delta$E (meV)', fontsize=17, labelpad=-1, fontweight='bold') 
+    axs.tick_params( axis="x", direction="in", width=3, length=9, labelsize=17, pad=4) 
+    axs.tick_params( axis="y", direction="in", width=3, length=9, labelsize=19, pad=4)  
+
+    axs.legend(fontsize=11, ncol=1, frameon=True, loc='upper left', bbox_to_anchor=(0.0, 1.0))
+    axs.spines['top'].set_linewidth(4)
+    axs.spines['bottom'].set_linewidth(4) 
+    axs.spines['left'].set_linewidth(4)
+    axs.spines['right'].set_linewidth(4)    
+    axs.set_box_aspect(1)
+
+    # axs.text(-45,41,f'MAE: {l2i5_E_mae:.2f}',verticalalignment='top', horizontalalignment='left', fontsize=10, color='black')
+    # axs.text(-45, 48, fr'$R^2$: {l2i5_E_r2:.2f}', verticalalignment='top', horizontalalignment='left', fontsize=10, color='black')
+    axs.annotate(fr'FCC-BCC  MAE: {fb_mae:.2f}, $R^2$: {fb_r2:.2f}',(0.98,0.23), bbox=dict(facecolor="white",edgecolor="none",alpha=0.7),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')
+    axs.annotate(fr'FCC-HCP  MAE: {fh_mae:.2f}, $R^2$: {fh_r2:.2f}',(0.98,0.18),bbox=dict(facecolor="white",edgecolor="none",alpha=0.7),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')
+    axs.annotate(fr'HCP-BCC  MAE: {hb_mae:.2f}, $R^2$: {hb_r2:.2f}',(0.98, 0.13),bbox=dict(facecolor="white",edgecolor="none",alpha=0.7),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')    
+    axs.annotate(fr'TOTAL  MAE: {mae:.2f},   $R^2$: {r2:.2f}',(0.98,0.08),bbox=dict(facecolor="white",edgecolor="none",alpha=0.7),xycoords='axes fraction',va='top', horizontalalignment='right', fontsize=13, color='black')
+
+    axs.set_xlim(-1000,650)
+    axs.set_ylim(-1000,650)
+    axs.set_xticks([-750, -500, -250, 0, 250, 500])
+    axs.set_yticks([-750, -500, -250, 0, 250, 500])
+    fig.set_layout_engine('tight')
+
+    tag=f"{df['bravais'].to_list()[0]}{df['bravais'].to_list()[1]}"
+    
+    path=make_dir(os.path.join(os.environ['PLOT'],'delE',system.lower()), return_path=True)
+    fig.savefig(os.path.join(path,f'{system.lower()}+{tag}_{mlp}_delE.png'))
+    if return_scores:
+        return mae, r2
+    return
